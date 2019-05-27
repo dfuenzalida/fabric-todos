6ab5278045ddadc6de4241232f521269ce2826bb
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-05-27 13:58:20 -0700

    Header basics

--- src/fabric_todos/core.cljs
+++ src/fabric_todos/core.cljs
@@ -6,21 +6,36 @@
 
 (def Button js/Fabric.PrimaryButton)
 (def Rating js/Fabric.Rating)
+(def PrimaryButton js/Fabric.PrimaryButton)
 (def Text js/Fabric.Text)
+(def TextField js/Fabric.TextField)
+(def Stack js/Fabric.Stack)
 
-;; Views ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Components ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn home-page []
-  [:div.ms-Grid
-   [:div.ms-Grid-row
-    [:> Text {:variant "large"} "Welcome to Reagent"]]
+(defn todo-header []
+  [:> Stack
+   [:> Stack {:horizontal true :horizontalAlign "center"}
+    [:> Stack.Item {:align "center"}
+     [:> Text {:variant "xxLarge"} "todos"]]]
+   [:> Stack {:horizontal "horizontal"}
+    [:> Stack.Item {:grow true}
+     [:> TextField {:placeholder "What needs to be done?"}]]
+    [:> PrimaryButton "Add"]]
    ])
+
+(defn todo-app []
+  [:> Stack {:horizontalAlign "center"}
+   [:> Stack {:style {:width 400} :gap "25"}
+    (todo-header)
+    ]]
+  )
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (r/render [home-page] (.getElementById js/document "app")))
+  (r/render [todo-app] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
