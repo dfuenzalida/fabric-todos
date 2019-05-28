bb9c8d5160114521edf4f90e4199c0843a39e57c
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-05-27 17:52:45 -0700

    Adding TODOs and debugging

--- src/fabric_todos/core.cljs
+++ src/fabric_todos/core.cljs
@@ -13,15 +13,23 @@
 (def TextField js/Fabric.TextField)
 (def Stack js/Fabric.Stack)
 
-(defonce state (r/atom {}))
+(defonce state (r/atom {:todos [] :counter 0 :labelInput ""}))
+(def debug true) ;; using 'def' on purpose to throw state in between reloads
 ;; Components ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn pivot-filter [pivot-item]
   (println pivot-item.props.headerText))
 
 (defn textfield-change [event newValue]
-  (swap! state assoc-in [:labelInput] newValue)
-  (println newValue))
+  (swap! state assoc :labelInput newValue)
+  (when debug (println @state)))
+
+(defn add-todo []
+  (let [text (get @state :labelInput)
+        id   (:counter (swap! state update :counter inc)) ]
+    (swap! state assoc :labelInput "")
+    (swap! state update :todos conj {:id id :text text :done false})
+    (when debug (println @state))))
 
 (defn todo-header []
   [:> Stack
@@ -30,8 +38,10 @@
      [:> Text {:variant "xxLarge"} "todos"]]]
    [:> Stack {:horizontal "horizontal"}
     [:> Stack.Item {:grow true}
-     [:> TextField {:placeholder "What needs to be done?" :value (str "" (get-in @state [:labelInput])) :onChange textfield-change}]]
-    [:> PrimaryButton "Add"]]
+     [:> TextField {:placeholder "What needs to be done?"
+                    :value (get-in @state [:labelInput])
+                    :onChange textfield-change}]]
+    [:> PrimaryButton {:onClick add-todo} "Add"]]
 
    [:> Pivot {:onLinkClick pivot-filter}
     [:> PivotItem {:headerText "all"}]
