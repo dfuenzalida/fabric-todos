fa3d426b5cb403f16a77f056d31e95c27bc36e07
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-05-27 18:18:31 -0700

    wip todos-list

--- src/fabric_todos/core.cljs
+++ src/fabric_todos/core.cljs
@@ -1,10 +1,13 @@
 (ns fabric-todos.core
-  (:require [Fabric]
+  (:require [clojure.string :refer [blank?]]
+            [Fabric]
             [reagent.core :as r]))
 
 ;; Fabric elements ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def Button js/Fabric.PrimaryButton)
+(def Checkbox js/Fabric.Checkbox)
+(def IconButton js/Fabric.IconButton)
 (def Rating js/Fabric.Rating)
 (def Pivot js/Fabric.Pivot)
 (def PivotItem js/Fabric.PivotItem)
@@ -15,7 +18,8 @@
 
 (defonce state (r/atom {:todos [] :counter 0 :labelInput ""}))
 (def debug true) ;; using 'def' on purpose to throw state in between reloads
-;; Components ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Header ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn pivot-filter [pivot-item]
   (println pivot-item.props.headerText))
@@ -27,9 +31,10 @@
 (defn add-todo []
   (let [text (get @state :labelInput)
         id   (:counter (swap! state update :counter inc)) ]
-    (swap! state assoc :labelInput "")
-    (swap! state update :todos conj {:id id :text text :done false})
-    (when debug (println @state))))
+    (when-not (blank? text)
+      (swap! state assoc :labelInput "")
+      (swap! state update :todos conj {:id id :text text :editing false :done false})
+      (when debug (println @state)))))
 
 (defn todo-header []
   [:> Stack
@@ -39,7 +44,7 @@
    [:> Stack {:horizontal "horizontal"}
     [:> Stack.Item {:grow true}
      [:> TextField {:placeholder "What needs to be done?"
-                    :value (get-in @state [:labelInput])
+                    :value (:labelInput @state)
                     :onChange textfield-change}]]
     [:> PrimaryButton {:onClick add-todo} "Add"]]
 
@@ -49,15 +54,33 @@
     [:> PivotItem {:headerText "completed"}]]
    ])
 
+;; Todo List ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn todo-item [{:keys [id text editing done] :as item}]
+  [:> Stack {:key (str "stack" id) :horizontal true :verticalAlign "center" :horizontalAlign "space-between"}
+   (when-not editing
+     [:> Checkbox {:id id :key (str "check" id) :label text :checked done}
+      [:div
+       [:> IconButton {:iconProps {:iconName "Refresh"} :className "clearButton"}]
+       [:> IconButton {:iconProps {:iconName "Refresh"} :className "clearButton"}]
+       ]])])
+
+(defn todo-list []
+  [:> Stack {:gap 10}
+   (map todo-item (:todos @state))
+   ])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn todo-app []
   [:> Stack {:horizontalAlign "center"}
    [:> Stack {:style {:width 400} :gap "25"}
     (todo-header)
-    ]]
-  )
+    (todo-list)
+    ]
+   ])
 
-;; -------------------------
-;; Initialize app
+;; App initialization ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn mount-root []
   (r/render [todo-app] (.getElementById js/document "app")))
