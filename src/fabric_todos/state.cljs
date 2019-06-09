7fd548f9023b900aed603764ebdb0487ac9fd4a6
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-06-08 18:05:04 -0700

    Updated implementation

--- src/fabric_todos/state.cljs
+++ src/fabric_todos/state.cljs
@@ -3,25 +3,20 @@
 
 ;; Global app state ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defonce state (r/atom {:todos [] :counter 0 :labelInput "" :filter "all"}))
+(defonce state (r/atom {:todos (sorted-map) :counter 0 :labelInput "" :filter "all"}))
 (def debug false) ;; using 'def' on purpose to throw state in between reloads
 
 ;; State APIs ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn add-todo [text]
   (let [id (:counter (swap! state update :counter inc))]
-    (swap! state update :todos conj {:id id :text text :editing false :done false})))
+    (swap! state update :todos conj {id {:id id :text text :editing false :done false}})))
 
 (defn delete-todo [id]
-  (let [new-todos (into [] (remove #(= id (:id %)) (:todos @state)))]
-    (swap! state assoc :todos new-todos)))
+  (swap! state update-in [:todos] dissoc id))
 
 (defn toggle-attribute [id attr]
-  (let [todos (:todos @state)]
-    (swap! state assoc :todos
-           (mapv
-            #(if (= id (:id %)) (update-in % [attr] false?) %)
-            todos))))
+  (swap! state update-in [:todos id attr] false?))
 
 (defn edit-todo [id]
   (toggle-attribute id :editing))
@@ -37,8 +32,9 @@
   (swap! state assoc :filter label))
 
 (defn clear-completed []
-  (let [items (into [] (remove #(= true (:done %)) (:todos @state)))]
-    (swap! state assoc :todos items)))
+  (let [completed-ids (map first (filter #(get-in % [1 :done]) (:todos @state)))
+        active-todos  (reduce dissoc (:todos @state) completed-ids)]
+    (swap! state assoc :todos active-todos)))
 
 (defn update-label-input [new-value]
   (swap! state assoc :labelInput new-value)
@@ -48,10 +44,11 @@
   (:labelInput @state))
 
 (defn remaining-todos []
-  (remove #(= true (:done %)) (:todos @state)))
+  (map second
+       (remove #(= true (get-in % [1 :done])) (:todos @state))))
 
 (defn filtered-todos []
   (let [remove-map {"all" nil "active" true "completed" false}
         remove-on  (get remove-map (:filter @state))]
-    (remove #(= remove-on (:done %)) (:todos @state))))
+    (remove #(= remove-on (:done %)) (map second (:todos @state)))))
 
